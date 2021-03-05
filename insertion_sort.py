3f13ebb98342fc92ee4bb070783baf33f59cb489
Edgar <mmnledgar@gmail.com>
2021-03-05 13:38:57 +0000

    adding insertion_sort algorithm

---
+++ insertion_sort.py
@@ -0,0 +1,28 @@
+import random as m
+
+default_array=list()
+length_array=10
+
+def initializeAnArray(n,array):
+    for i in range(0,n):
+        array.append(m.randint(1,n+1))
+
+def displayTheArray(n,array):
+    for i in range (0,n):
+        print(array[i])
+
+def insertionSort(n,array):
+    for i in range(n):
+        for j in range(i):
+            if(array[j]<array[i]):
+                memory=array[i]
+                array[i]=array[j]
+                array[j]=memory
+
+print("=========unsorted array=============")
+initializeAnArray(length_array,default_array)
+displayTheArray(length_array,default_array)
+
+print("========== sorted array ===========")
+insertionSort(length_array,default_array)
+displayTheArray(length_array,default_array)

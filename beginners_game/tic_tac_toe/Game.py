ffc215c2b1747bba92f18837c499169dca26f6be
Edgar <mmnledgar@gmail.com>
2022-05-03 12:58:02 +0000

    adding tic tac toe game

--- beginners_game/tic_tac_toe/Game.py
+++ beginners_game/tic_tac_toe/Game.py
@@ -7,9 +7,28 @@ class Game:
         # from 0 to 2
         # from 3 to 5
         # from 6 to 8
-        rows = [self.board[i*3:(i+1)*3] for i in range(3)]
+        rows = [self.board[i * 3:(i + 1) * 3] for i in range(3)]
         for row in rows:
+            print(" | " + " | ".join(row) + " | ")
+
+    @staticmethod
+    def print_board_nums():
+        rows_number = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
+        # equivalent
+        # rows = []
+        # for j in range(3):
+        #     inter = []
+        #     for i in range(j*3, (j+1)*3):
+        #         inter.append(str(i))
+        #     rows.append(inter)
+        # print(rows)
+        for row in rows_number:
+            print(" | " + " | ".join(row) + " | ")
+
+    def available_move(self):
+        moves = [i for (i, spot) in enumerate(self.board) if spot == ' ']
+        return moves
 
 
 game = Game()
-game.print_board()
+game.available_move()

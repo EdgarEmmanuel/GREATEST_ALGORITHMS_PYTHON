c7549325155122a407332b8f9d8e4cd250c22440
Edgar <mmnledgar@gmail.com>
2022-05-03 12:00:19 +0000

    adding qr code generator project

---
+++ beginners_game/qrcode_generator/README.md
@@ -0,0 +1,9 @@
+# QR CODE GENERATOR AND DECODER
+___
+
+
+## Tips : [install pyzbar on linux](https://stackoverflow.com/questions/48792965/importerror-unable-to-find-zbar-shared-library-on-flask)
+
+___
+## QRCODE
+![data]()
--- beginners_game/qrcode_generator/qrcode_generator.py
+++ beginners_game/qrcode_generator/qrcode_generator.py
@@ -1,20 +1,20 @@
 import qrcode
-from qrtools import qrtools
+#from qrtools import qrtools
 from PIL import Image
 from pyzbar.pyzbar import decode
 
 
 # =============== encode data ==============
 
-def generate_qr_code(username):
-    img = qrcode.make(username)
-    img.save(f"{username}.png")
+def generate_qr_code(filename, data):
+    img = qrcode.make(data)
+    img.save(f"{filename}.png")
     print("QR CODE CREATED SUCCESSFULLY")
 
 
 userName = input("Type your name: ")
-final = userName.replace("","_")
-generate_qr_code(final)
+final = userName.replace(" ","_")
+generate_qr_code(final, userName)
 
 
 # =========== decode data ===============
@@ -23,8 +23,8 @@ generate_qr_code(final)
 def decode_qr_image(imagePath):
     #qr = qrtools.QR()
     data = decode(Image.open(imagePath))
-    print(f"the content of the QR image id : {data}")
+    print(f"the content of the QR image id : {data.data}")
 
 
-final = userName.replace("","_")
+final = userName.replace(" ","_")
 decode_qr_image(f"{final}.png")

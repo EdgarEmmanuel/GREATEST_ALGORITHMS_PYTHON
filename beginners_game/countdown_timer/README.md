e9d2c38705dbc2e2147faa1d1bc6c2f7cd5d15ae
Edgar <mmnledgar@gmail.com>
2022-05-03 11:24:21 +0000

    adding games

---
+++ beginners_game/countdown_timer/README.md
@@ -0,0 +1,4 @@
+# COUNTDOWN TIMER (COMMAND LINE)
+___
+
+![image](countdwon.png)
---
+++ beginners_game/countdown_timer/countdown.py
@@ -0,0 +1,17 @@
+import time
+
+
+timeInSecond = float(input("Type the time in seconds : "))
+
+date = time.gmtime(timeInSecond)
+
+totalTime = timeInSecond
+
+dict = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "saturday", 6: "Sunday"}
+
+while totalTime != 0:
+    date = time.gmtime(timeInSecond)
+    print(f"{dict.get(date[6])}/{date[1]}/{date[0]} , {date[3]}:{date[4]}:{date[5]}")
+    time.sleep(1)
+    timeInSecond -= 1
+    totalTime = timeInSecond

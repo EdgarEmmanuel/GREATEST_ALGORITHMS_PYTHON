12aad731a086dd3d5ea928d05722a9c7bdb876c0
Edgar <mmnledgar@gmail.com>
2022-05-03 12:32:06 +0000

    adding tic tac toe game

---
+++ beginners_game/tic_tac_toe/Player.py
@@ -0,0 +1,26 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter x or o
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+
+class ComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass

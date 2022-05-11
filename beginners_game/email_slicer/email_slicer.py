89aff83062ad45a22b642ad410e9c9d01d4ef113
Edgar <mmnledgar@gmail.com>
2022-05-11 11:14:46 +0000

    adding game

--- beginners_game/email_slicer/email_slicer.py
+++ beginners_game/email_slicer/email_slicer.py
@@ -3,20 +3,19 @@
 class User:
     def __init__(self, email):
         self.email = email
-        self.delimiterSymbol = None
+        self.delimiterSymbol = self.email.index("@")
 
     def determineEmailDomain(self):
-        self.delimiterSymbol = self.email.index("@")
         return self.email[self.delimiterSymbol+1:]
 
     def determineEmailUserName(self):
-        self.delimiterSymbol = self.email.index("@")
         return self.email[:self.delimiterSymbol]
 
     def printUserEmailInfo(self):
         username = self.determineEmailUserName()
         domain = self.determineEmailDomain()
-        print(f"Your username is {username} and the domain is {domain}")
+        print(f"Your Email Is : {self.email}")
+        print(f"Your username is << {username} >> and the domain is << {domain} >>")
 
 
 if __name__ == "__main__":

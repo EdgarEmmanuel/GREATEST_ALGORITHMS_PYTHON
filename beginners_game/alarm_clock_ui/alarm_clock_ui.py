1f322b12829949ecec4f15e1f7f886dd8259d3b8
Edgar <mmnledgar@gmail.com>
2022-05-11 11:55:17 +0000

    adding game

---
+++ beginners_game/alarm_clock_ui/alarm_clock_ui.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import time
+import tkinter.font as tk_font
+root = Tk()
+
+helv = tk_font.Font(family="Helvetica",size=20,weight="bold")
+font_arial = tk_font.Font(family='Arial', size=24, weight='normal', slant='italic', underline=1)
+
+
+root.title("Alarm Clock UI")
+
+root.geometry("500x250")
+frame = Frame(root)
+frame.pack()
+
+label = Label(frame, text="Alarm Clock", font=font_arial)
+label.pack()
+
+labelDate = Label(frame, justify=CENTER, font=helv)
+labelDate.pack(padx=3, pady=10)
+
+def clock():
+    date = time.localtime()
+    dict = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "saturday", 6: "Sunday"}
+    timeFormatted = f"{dict.get(date[6])} / {date[1]} / {date[0]} , {date[3]}:{date[4]}:{date[5]}"
+    labelDate.config(text=timeFormatted)
+    root.after(1000, clock)
+
+clock()
+root.mainloop()
